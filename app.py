d699ec4d84a791efcedff056d49d28f8f1c68484
safiraauliar <safiraauliarohmah@gmail.com>
2022-01-31 19:15:23 +0700

    add file

---
+++ app.py
@@ -0,0 +1,24 @@
+from crypt import methods
+import flask
+import numpy as np
+import pickle
+
+model = pickle.load(open('model/lr_model_predict.pkl','rb'))
+
+app = flask.Flask(__name__, template_folder='templates')
+
+@app.route('/')
+def main():
+    return(flask.render_template('main.html'))
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    features = [float(x) for x in flask.request.form.values()]
+    final_features = [np.array(features)]
+    prediction = model.predict(final_features)
+
+    output = {0.0:'Tidak Akan Hujan', 1.0:'Akan Hujan'}
+    return flask.render_template('main.html',prediction_text='Kemungkinan Besok {}'.format(output[prediction[0]]))
+
+if __name__=='__main__':
+    app.run(debug=True)
