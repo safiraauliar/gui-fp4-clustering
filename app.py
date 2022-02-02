df52038babab39cc3ffc69934a7243d783071990
safiraauliar <safiraauliarohmah@gmail.com>
2022-02-02 15:39:04 +0700

    push file

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import flask
 import numpy as np
 import pickle
 
-model = pickle.load(open('model/lr_model_predict.pkl','rb'))
+model = pickle.load(open('model/RF_model_predict.pkl','rb'))
 
 app = flask.Flask(__name__, template_folder='templates')
 
@@ -17,8 +17,8 @@ def predict():
     final_features = [np.array(features)]
     prediction = model.predict(final_features)
 
-    output = {0.0:'Tidak Akan Hujan', 1.0:'Akan Hujan'}
-    return flask.render_template('main.html',prediction_text='Kemungkinan Besok {}'.format(output[prediction[0]]))
+    output = {0:'Belum Meninggal', 1:'Sudah Meninggal'}
+    return flask.render_template('main.html',prediction_text='Pasien {}'.format(output[prediction[0]]))
 
 if __name__=='__main__':
     app.run(debug=True)
